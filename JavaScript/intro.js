64052915dc142597a9b8bd7148d187f781ccbb54
PrajaktaGarodi <prajaktagarodi@gmail.com>
2024-07-16 09:14:59 +0530

    upload

---
+++ JavaScript/intro.js
@@ -0,0 +1,17 @@
+alert("hello would");
+console.log("hey i am console")
+console.log("looking like a wow ")
+
+
+var a = prompt("Enter Your Number")
+console.log("your no is " + a)
+
+var isTrue = confirm("Are you sure you want to leave this page and blast your computer")
+
+if(isTrue){
+    console.log("Computer is Blasting")
+}
+else{
+    console.log("omputer is not blasting ")
+}
+console.log("Your number is "+ a)
